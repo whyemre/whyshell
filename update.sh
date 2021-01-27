a9eb343f37417f5d1b14b8dc94146452754756d4
whyemre <76700007+whyemre@users.noreply.github.com>
2021-01-27 15:02:45 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ clear
 printf "\n \e[1;92mDizin \e[1;94mShellPhish\e[1;92m güncelleniyor ...\n\n"
 sleep 1.5
 cd ..
-rm -rf whyshell-
+rm -rf ShellPhish
 git clone https://github.com/whyemre/whyshell-
 cd whyshell-
 chmod +x *
